89b3e4a0217473303b532a4c1c273abbe4171edd
leonardo.cigula@gmail.com <109247428+leoCigula@users.noreply.github.com>
2024-10-22 15:17:44 +0200

    izbacivanje komentara i newlines

--- Labos1/src/hr/fer/ppj/LeksickiAnalizator.java
+++ Labos1/src/hr/fer/ppj/LeksickiAnalizator.java
@@ -7,14 +7,31 @@ public class LeksickiAnalizator {
     public String keywords[]={"za","od"," do","az"};
     public static void main(String args[]){
         String ulaz;
-        System.out.println("Pozdrav svijete");
+        String program;
 
         try{
 
             BufferedReader citac = new BufferedReader(new InputStreamReader(System.in));
+            StringBuilder sb = new StringBuilder();
             while(!((ulaz=citac.readLine()).isBlank())){
-                System.out.println(ulaz);
+                    sb.append(ulaz.concat("\n"));
             }
+            //program = new String(sb.toString());
+            //System.out.println(program);
+
+            //izbacivanje komentara
+            int pos=0;
+            while( sb.indexOf("//") > -1){
+                    pos=sb.indexOf("\n",sb.indexOf("//"));
+                    sb.delete(sb.indexOf("//"),pos+1);
+                    //System.out.printf("%d %d", i, program.indexOf('\n', i))
+            }
+            //izbaceni komentari
+            program = new String(sb.toString().trim());
+
+            System.out.println(program);
+
+
 
         }
         catch (Exception e){
@@ -22,4 +39,6 @@ public class LeksickiAnalizator {
         }
 
     }
+
+
 }
