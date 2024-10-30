cb52d91099ebdbadb34ca201c790264aebaa8e23
leonardo.cigula@gmail.com <109247428+leoCigula@users.noreply.github.com>
2024-10-30 17:27:14 +0100

    Pocetak 2. labosa iz PPJ

---
+++ Labos2/src/hr/fer/ppj/SintaksniAnalizator.java
@@ -0,0 +1,13 @@
+package hr.fer.ppj;
+
+public class SintaksniAnalizator {
+
+    public static void main(String args[]){
+
+        //klasa za uvod u program
+        System.out.println("Pozdrav PPj lab 2");
+
+    }
+
+}
+
